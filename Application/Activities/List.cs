1ff16edb4187804f860e3c584f6b268a741e717b
sntshred <santosh.aryan9@gmail.com>
2023-08-20 02:59:08 -0400

    added CQRS pattern

---
+++ Application/Activities/List.cs
@@ -0,0 +1,45 @@
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+            private readonly ILogger _logger;
+
+            public Handler(DataContext context, ILogger<List> logger)
+            {
+                _context = context;
+                _logger = logger;
+            }
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                        await Task.Delay(1000, cancellationToken);
+                        _logger.LogInformation($"Task  {i} Completed");
+                    }
+                }
+                catch (System.Exception)
+                {
+
+                    _logger.LogInformation("Task throw: cancelled");
+
+                }
+
+                return await _context.Activities.ToListAsync();
+            }
+        }
+    }
+}
